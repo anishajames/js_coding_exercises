e46b83c107668b33d751a7beee7a2d26dd9af10d
Anisha James <73602224+anishajames@users.noreply.github.com>
2020-11-29 22:31:36 +0000

    implemented getSquares

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -1,6 +1,14 @@
 function getSquares(nums) {
   if (nums === undefined) throw new Error("nums is required");
-  // Your code here!
+  if(nums.length == 0)  
+    return nums;
+  else{
+    for (i = 0; i < nums.length; i++) {
+      nums[i] = nums[i]*nums[i];
+    }
+    return nums;
+  } 
+
 }
 
 function camelCaseWords(words) {
