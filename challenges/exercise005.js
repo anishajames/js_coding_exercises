e5940842ad34dcaa154fbc8979944695d90b01c3
Anisha James <73602224+anishajames@users.noreply.github.com>
2020-12-22 01:14:29 +0000

    implemented sumArrays in exercise005.js

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -19,7 +19,7 @@ const reverseNumber = (n) => {
 
 const sumArrays = (arrs) => {
   if (arrs === undefined) throw new Error("arrs is required");
-  // Your code here!
+  return arrs.flat().reduce((a, b) => a + b);
 };
 
 const arrShift = (arr) => {
